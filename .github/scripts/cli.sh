74fe0ab3ad843276806212132489b99b841a2e49
Andreas Bigger <abigger87@gmail.com>
2023-01-26 17:38:04 -0500

    fix: copy rosetta-cli over :zap:

--- .github/scripts/cli.sh
+++ .github/scripts/cli.sh
@@ -5,7 +5,9 @@ echo "Downloading rosetta-cli..."
 curl -sSfL https://raw.githubusercontent.com/coinbase/rosetta-cli/master/scripts/install.sh | sh -s
 echo "rosetta-cli downloaded"
 
+echo "Copying rosetta-cli to root..."
 cp ./bin/rosetta-cli ./
+echo "Rosetta cli location: $(which rosetta-cli)"
 
 # echo "start check:data"
 # ./bin/rosetta-cli --configuration-file examples/ethereum/rosetta-cli-conf/devnet/config.json check:data --start-block 0
