f864483680567aa3efed1511826ce84660238c65
Andreas Bigger <abigger87@gmail.com>
2023-01-26 17:21:45 -0500

    fix: install rosetta-cli

---
+++ .github/scripts/cli.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# downloading cli
+echo "Downloading rosetta-cli..."
+curl -sSfL https://raw.githubusercontent.com/coinbase/rosetta-cli/master/scripts/install.sh | sh -s
+echo "rosetta-cli downloaded"
+
+# echo "start check:data"
+# ./bin/rosetta-cli --configuration-file examples/ethereum/rosetta-cli-conf/devnet/config.json check:data --start-block 0
