f880d5e56707048089787a57631afe680e9a60b6
Andreas Bigger <abigger87@gmail.com>
2023-01-27 09:17:23 -0500

    fix: github actions with custom scripts :construction:

--- .github/scripts/validate-goerli-config.sh
+++ .github/scripts/validate-goerli-config.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-./.github/scripts/cli.sh
+echo "Downloading rosetta-cli..."
+curl -sSfL https://raw.githubusercontent.com/coinbase/rosetta-cli/master/scripts/install.sh | sh -s
+echo "rosetta-cli downloaded"
+./bin/rosetta-cli version
 
 ./bin/rosetta-cli configuration:validate configs/optimism/goerli.json
