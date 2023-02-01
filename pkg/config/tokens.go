966d9c581cf135a43531de7944e03f0218a9ba89
Andreas Bigger <abigger87@gmail.com>
2023-02-01 18:19:54 -0500

    graceful token list parsing

---
+++ pkg/config/tokens.go
@@ -0,0 +1,63 @@
+package config
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/coinbase/rosetta-geth-sdk/configuration"
+)
+
+// ReadTokenConfig attempts to read file contents from a filename.
+func ReadTokenConfig(filename string) (string, error) {
+	contents := ""
+	if file, err := os.ReadFile(filename); err == nil {
+		// if the envvar points to a file, read it; otherwise the envvar contents is expected to be JSON
+		contents = string(file)
+	}
+	if contents == "" {
+		contents = "[]"
+	}
+	return contents, nil
+}
+
+// UnmarshalTokenConfig attempts to construct a list of [configuration.Token] from a JSON file.
+func UnmarshalTokenConfig(contents string) ([]configuration.Token, error) {
+	// Try to parse the file as a list of tokens.
+	var payload []configuration.Token
+	if err := json.Unmarshal([]byte(contents), &payload); err == nil {
+		return payload, nil
+	}
+
+	// If this fails, try the backwards-compatible token json format
+	var outer map[string]interface{}
+	if err := json.Unmarshal([]byte(contents), &outer); err == nil {
+		for k, v := range outer {
+			for t, _ := range v.(map[string]interface{}) {
+				fmt.Printf("t: %s\n", t)
+				switch k {
+				case "Mainnet":
+					payload = append(payload, configuration.Token{
+						ChainID: 1,
+						Address: t,
+					})
+				case "Testnet":
+					payload = append(payload, configuration.Token{
+						ChainID: 420,
+						Address: t,
+					})
+				case "Goerli":
+					payload = append(payload, configuration.Token{
+						ChainID: 420,
+						Address: t,
+					})
+				default:
+					return nil, fmt.Errorf("unknown chain %s found when parsing json token list", k)
+				}
+			}
+		}
+		return payload, nil
+	}
+
+	return nil, fmt.Errorf("error parsing file contents as json token list")
+}
---
+++ pkg/config/tokens_test.go
@@ -0,0 +1,94 @@
+package config_test
+
+import (
+	"testing"
+
+	SdkConfiguration "github.com/coinbase/rosetta-geth-sdk/configuration"
+	"github.com/mdehoog/op-rosetta/pkg/config"
+
+	"github.com/stretchr/testify/suite"
+)
+
+// TokensTestSuite is a test suite for tokens utilities.
+type TokensTestSuite struct {
+	suite.Suite
+}
+
+// TestTokens runs the TokensTestSuite.
+func TestTokens(t *testing.T) {
+	suite.Run(t, new(TokensTestSuite))
+}
+
+// TestUnmarshalOldTokenConfig tests unmarshalling the outdated token config json format.
+func (testSuite *TokensTestSuite) TestUnmarshalOldTokenConfig() {
+	contents := `{
+		"Mainnet" : {
+			"0x4200000000000000000000000000000000000042": true,
+			"0x7f5c764cbc14f9669b88837ca1490cca17c31607": true
+		},
+		"Testnet" : {
+			"0xda10009cbd5d07dd0cecc66161fc93d7c9000da1": true
+		},
+		"Goerli" : {
+			"0x7f5c764cbc14f9669b88837ca1490cca17c31607": true
+		}
+	}`
+	c, err := config.UnmarshalTokenConfig(contents)
+	testSuite.NoError(err)
+	testSuite.Equal([]SdkConfiguration.Token{
+		{
+			ChainID: 1,
+			Address: "0x4200000000000000000000000000000000000042",
+		},
+		{
+			ChainID: 1,
+			Address: "0x7f5c764cbc14f9669b88837ca1490cca17c31607",
+		},
+		{
+			ChainID: 420,
+			Address: "0xda10009cbd5d07dd0cecc66161fc93d7c9000da1",
+		},
+		{
+			ChainID: 420,
+			Address: "0x7f5c764cbc14f9669b88837ca1490cca17c31607",
+		},
+	}, c)
+}
+
+// TestUnmarshalTokenConfig tests unmarshalling the token config json format.
+func (testSuite *TokensTestSuite) TestUnmarshalTokenConfig() {
+	contents := `[
+		{
+			"chainId": 1,
+			"address": "0x7f5c764cbc14f9669b88837ca1490cca17c31607",
+			"name": "USD Coin",
+			"symbol": "USDC",
+			"decimals": 6
+		},
+		{
+			"chainId": 1,
+			"address": "0x94b008aa00579c1307b0ef2c499ad98a8ce58e58",
+			"name": "USDT",
+			"symbol": "USDT",
+			"decimals": 6
+		}
+	]`
+	c, err := config.UnmarshalTokenConfig(contents)
+	testSuite.NoError(err)
+	testSuite.Equal([]SdkConfiguration.Token{
+		{
+			ChainID:  1,
+			Address:  "0x7f5c764cbc14f9669b88837ca1490cca17c31607",
+			Name:     "USD Coin",
+			Symbol:   "USDC",
+			Decimals: 6,
+		},
+		{
+			ChainID:  1,
+			Address:  "0x94b008aa00579c1307b0ef2c499ad98a8ce58e58",
+			Name:     "USDT",
+			Symbol:   "USDT",
+			Decimals: 6,
+		},
+	}, c)
+}
